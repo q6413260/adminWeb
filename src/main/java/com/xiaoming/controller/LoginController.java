ea730b69d3135d9d346814468047dc76da142451
xiaoming <xiaoming@xiaomings-MacBook-Pro.local>
2016-11-22 18:46:59 +0800

    upload source

--- src/main/java/com/xiaoming/controller/LoginController.java
+++ src/main/java/com/xiaoming/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.xiaoming.controller;
 
 import com.xiaoming.dto.UserDTO;
 import com.xiaoming.service.UserService;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -35,17 +37,23 @@ public class LoginController {
             mav.addObject("errorMsg", "账号或者密码错误");
             return mav;
         }
-        mav = new ModelAndView("redirect:/hello.vm");
+        mav = new ModelAndView("redirect:/developer.vm");
         Cookie cookie =new Cookie("token", token);
+        cookie.setMaxAge(7*24*60*60);
         response.addCookie(cookie);
         return mav;
     }
 
-//    @RequestMapping(value = "/login")
-//    public ModelAndView printWelcome() {
-//        ModelAndView mav = new ModelAndView();
-//        mav.addObject("city", "test");
-//        mav.setViewName("login");
-//        return mav;
-//    }
+    @RequestMapping(value = "logout", method = RequestMethod.GET)
+    public ModelAndView logout(HttpServletRequest request, HttpServletResponse response){
+        ModelAndView mav = new ModelAndView("redirect:/login.vm");
+        Cookie[] cookies = request.getCookies();
+        if(!ArrayUtils.isEmpty(cookies)){
+            for(Cookie cookie : cookies){
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+        }
+        return mav;
+    }
 }
